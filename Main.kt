87c6e0482481493c6b89b2a905d02d01c2d4ef0a
Amar sulais <Amarinho@Amars-MacBook-Pro-2.local>
2021-10-16 19:39:02 +0300

    HW_week1_Day4_FunctionsAndStrings

---
+++ Main.kt
@@ -0,0 +1,60 @@
+
+fun main() {
+    var a= arrayOf("Ali")
+    printMiddleCharacter(a)
+}
+fun printMiddleCharacter(a:Array<String>){
+    for (x in a){
+        if (x.length<3){
+            println(x)
+        }
+    }
+}
+//////////////////////////////////////////////////////////
+
+fun main() {
+    var a= arrayOf("supermarket")
+    printVowels(a)
+}
+fun printVowels(a:Array<String>){
+    for (x in a){
+        if (x.vowlCount){
+            println(printVowels())
+        }
+    }
+}
+///////////////////////////////////////////////////////////////
+
+fun main() {
+    var c= arrayOf("isEven")
+    println(){
+
+    }
+}
+
+fun main() {
+
+    println("Please enter a number ")
+    var c = Integer.valueOf(readLine())
+
+    if (c ==2) {
+
+        println("True")
+    }else{
+        println("False")
+    }
+}
+
+////////////////////////////////////////////////////////////////
+
+fun main() {
+    var R= arrayOf("Amar sulais")
+    println(R)
+
+}
+fun printRemoveSpace(R:Array<String>){
+    for (x in R)
+        if (String=R) println(printRemoveSpace())
+}
+
+
